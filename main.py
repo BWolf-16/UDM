324e2f0000ef4d4a359a075197494725b824007f
BWolf <benedikt.vnvdl+github@gmail.com>
2025-05-28 22:15:02 +0200

    Refactor admin check

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 # main.py
+import os
+import sys
+import ctypes
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    # Relaunch as admin
+    script = os.path.abspath(sys.argv[0])
+    params = " ".join([f'"{arg}"' for arg in sys.argv[1:]])
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f'"{script}" {params}', None, 1)
+    sys.exit()
+
+# After admin check passed
 from ui.app import launch_app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     launch_app()
